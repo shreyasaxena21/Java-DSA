8929f818fb565190a2e276d15147294bd852e017
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-05-28 18:03:52 +0000

    reverse array

---
+++ Array/ReverseArray.java
@@ -0,0 +1,16 @@
+class ReverseArray{
+    public static void main(String args []){
+        int [] arr = {10,20,30,40,50};
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+    }
+
+}
