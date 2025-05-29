c8ca575e26565f9f81413e1888c8551753c77260
Shreya Saxena <172243056+shreyasaxena21@users.noreply.github.com>
2025-05-29 15:11:31 +0000

    java

---
+++ Array/Professor Party.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+
+    public static String PartyType(int arr[]) {
+        // Your code goes here
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        
+        for(int val : arr){
+            if(map.get(val)==null){
+                map.put(val, true);
+            }
+            
+            else{
+                return "true";
+            }
+            
+        }
+        
+        return "false";
+    }
+}
